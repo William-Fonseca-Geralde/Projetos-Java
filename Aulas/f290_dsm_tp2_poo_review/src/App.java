7d15ace90f190f3de91d962d2eaee7cabc15fe28
William-Fonseca-Geralde <127157689+William-Fonseca-Geralde@users.noreply.github.com>
2024-02-28 21:25:26 -0300

    impedir que idade seja menor que zero (versão 1)

--- Aulas/f290_dsm_tp2_poo_review/src/App.java
+++ Aulas/f290_dsm_tp2_poo_review/src/App.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 import domain.Pessoa;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
         Pessoa Roberto = new Pessoa("Roberto", 19, 185);
+        int idade = in.nextInt();
+        if (idade <= 0) {
+            System.out.println("A idade não pode ser menor que zero");
+        } else {
+            Roberto.idade = idade;
+        }
         System.out.println(Roberto);
+        in.close();
     }
 }
