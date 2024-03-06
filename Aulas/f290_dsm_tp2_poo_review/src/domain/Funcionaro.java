7f7ed804d6521420d1f8576885d10fce915b90ea
William-Fonseca-Geralde <127157689+William-Fonseca-Geralde@users.noreply.github.com>
2024-03-06 19:35:18 -0300

    adição das classes Professor e Funcionaro

---
+++ Aulas/f290_dsm_tp2_poo_review/src/domain/Funcionaro.java
@@ -0,0 +1,7 @@
+package domain;
+
+public class Funcionaro extends Pessoa {
+    public Funcionaro(String nome, int idade, int altura) {
+        super(nome, idade, altura);
+    }
+}
---
+++ Aulas/f290_dsm_tp2_poo_review/src/domain/Professor.java
@@ -0,0 +1,7 @@
+package domain;
+
+public class Professor extends Pessoa {
+    public Professor(String nome, int idade, int altura) {
+        super(nome, idade, altura);
+    }
+}
