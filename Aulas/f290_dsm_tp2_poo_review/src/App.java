9a6c0a9e322e9198805d6c7949beb8dacc4c78be
William-Fonseca-Geralde <127157689+William-Fonseca-Geralde@users.noreply.github.com>
2024-03-06 22:13:18 -0300

    pequenas incrementações

--- Aulas/f290_dsm_tp2_poo_review/src/App.java
+++ Aulas/f290_dsm_tp2_poo_review/src/App.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
 
+import domain.Estagiario;
 import domain.Estudante;
 import domain.Funcionaro;
 import domain.Pessoa;
@@ -11,10 +12,13 @@ public class App {
         Estudante Robs = new Estudante("Roberto", 19, 185);
         Professor Prof = new Professor("Nilton", 36, 185);
         Funcionaro Func = new Funcionaro("Aroldo", 41, 178);
+        Estagiario Estag = new Estagiario("Iara", 22, 170);
         
-        List<Pessoa> pessoas = Arrays.asList(Robs, Prof, Func);
+        List<Pessoa> pessoas = Arrays.asList(Robs, Prof, Func, Estag);
 
         for (Pessoa pessoa : pessoas) {
+            System.out.println();
+            System.out.println(pessoa);
             pessoa.Trabalhar();
         }
     }
