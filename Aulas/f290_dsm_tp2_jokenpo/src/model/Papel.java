8dbdf3f3fc2b2fb05777ac90b78f188b64f38ea4
William-Fonseca-Geralde <127157689+William-Fonseca-Geralde@users.noreply.github.com>
2024-03-13 21:52:32 -0300

    criação do projeto Jokenpo

---
+++ Aulas/f290_dsm_tp2_jokenpo/src/model/Papel.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Papel extends Algoritmo {
+
+    @Override
+    public void executar(Tipo jogadaComputador) {
+        switch (jogadaComputador) {
+            case Papel:
+                
+            break;
+            default:
+                break;
+        }
+    }
+    
+}
---
+++ Aulas/f290_dsm_tp2_jokenpo/src/model/Tipo.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum Tipo {
+    Papel, Tesoura, Pedra, Lagarto, Spock;
+}
